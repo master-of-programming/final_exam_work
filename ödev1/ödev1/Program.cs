9a94f36cc36cd761b7f6502b77566df3d567e6f0
master-of-programming <67226627+master-of-programming@users.noreply.github.com>
2020-06-26 02:56:07 +0300

    Add files via upload

---
+++ ödev1/ödev1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnaSınıf anaSınıf = new AnaSınıf();
+            anaSınıf.AnaMetod();
+            Console.ReadKey();
+        }
+    }
+    public class DikÜçgen{
+       public  int akenar, bkenar;
+        double hipo;
+        public void Alan(int a, int b) {
+            a = akenar;b = bkenar;
+            double alan = (a * b)/ 2;
+            Console.WriteLine("Alanı : " + alan);
+        }
+        public void Cevre(int a, int b) {
+            Hipo(a,b);
+            double cevre = a + b + hipo;
+            Console.WriteLine("Çevresi : " + cevre);
+        }
+        public void Hipo(int a, int b) {
+            hipo = Math.Sqrt(a * a + b * b);
+            Console.WriteLine("Hipotenüs : " + hipo);
+        }
+        public void Yaz() {
+            Console.WriteLine("A kenarı : "+akenar);
+            Console.WriteLine("B kenarı : "+bkenar);
+            Alan(akenar,bkenar);
+            Cevre(akenar, bkenar);
+        }
+    }
+    public class AnaSınıf
+    {
+        public void AnaMetod()
+        {
+            DikÜçgen dik = new DikÜçgen();
+            dik.akenar = 30;
+            dik.bkenar = 40;
+            dik.Yaz();
+        }
+    }
+}
---
+++ ödev1/ödev2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İlk matrisin satır sayısını girin : ");
+            int masat = Convert.ToInt32(Console.ReadLine());
+            Console.Write("İlk matrisin sütun sayısını girin : ");
+            int masut = Convert.ToInt32(Console.ReadLine());
+            Console.Write("İkinci matrisin satır sayısını girin : ");
+            int mbsat = Convert.ToInt32(Console.ReadLine());
+            Console.Write("İkinci matrisin sütun sayısını girin : ");
+            int mbsut = Convert.ToInt32(Console.ReadLine());
+            int[,] matrisA = new int[masat, masut];
+            int[,] matrisB = new int[mbsat, mbsut];
+            Random rnd = new Random();
+            Console.WriteLine("**************İLK MATRİS*****************");
+            for (int i = 0; i < masat; i++)
+            {
+                for (int j = 0; j < masut; j++)
+                {
+                    matrisA[i,j]=rnd.Next(0,12);
+                    Console.Write(matrisA[i, j]+"\t" );
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("********İKİNCİ MATRİS****************");
+            for (int k = 0; k < mbsat; k++)
+            {
+                for (int l = 0; l < mbsut; l++)
+                {
+                    matrisB[k,l] = rnd.Next(0, 12);
+                    Console.Write(matrisB[k,l] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("**************SKALER ÇARPIMI***********************");
+            int[,] matrisC = new int[matrisA.GetUpperBound(0) + 1, matrisB.GetUpperBound(1) + 1];
+            for (int a = 0; a <=matrisA.GetUpperBound(0); a++)
+            {
+                for (int b = 0; b <= matrisB.GetUpperBound(1); b++)
+                {
+                    int toplam =0;
+                    for (int c = 0; c <=matrisA.GetUpperBound(1); c++)
+                    {
+                        toplam += matrisA[a, c] * matrisB[c, b];
+                    }
+                    matrisC[a, b] = toplam;
+                    Console.Write(matrisC[a, b] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
